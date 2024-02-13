80b95b6ed2c99b21f4ea971ea94fda3f1fa83cb9
HallisCode <applgoolhal@gmail.com>
2024-02-13 21:48:43 +0300

    checking dnd kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 
-import MainField from "./shape-game/MainField.tsx";
-import SelectorField from "./shape-game/SelectorField.tsx";
+import ShapeGame from "./shape-game/ShapeGame.tsx";
 
-import styles from "./css/game.module.css"
 
 function App() 
 {
   return (
     <div className="App">
-      <div className={styles.MainField}>
-        <MainField/>
-      </div>
-      <div className={styles.SelectorField}>
-        <SelectorField/>
-      </div>
+      <ShapeGame/>
     </div>
   );
 }
